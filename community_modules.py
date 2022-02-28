ebc1f698a8b12c2598fa231c71c372a7aaf65ad5
Krzysztof Galus <krzysztof.galus.k7@gmail.com>
2022-02-28 11:53:48 +0100

    refactor: new structure

--- community_modules.py
+++ community_modules.py
@@ -1,11 +1,11 @@
 import discord
+
 from community_modules.GrzechuG.helloworld import *
 
-async def community_main(message):
-	
-	# Added by GrzechuG:
-	if "test community modules" in message.content:
-		await message.channel.send(hello_test(message.content))
-		return True
-	
 
+async def community_main(message):
+
+    # Added by GrzechuG:
+    if "test community modules" in message.content:
+        await message.channel.send(hello_test(message.content))
+        return True
