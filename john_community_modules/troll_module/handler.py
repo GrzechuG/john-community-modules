d077d37f12e9741cdcf9b10733bc5dbd91c61f77
Krzysztof Galus <krzysztof.galus.k7@gmail.com>
2022-03-01 15:41:24 +0100

    example module creating

---
+++ john_community_modules/troll_module/handler.py
@@ -0,0 +1,12 @@
+from typing import Optional, Any
+
+from john_community_modules.base import BaseEventHandler,  Message
+
+
+class TrollHandler(BaseEventHandler):
+    def __init__(self, troll_message: str = '💩'):
+        super().__init__()
+        self.troll_message = troll_message
+
+    def __call__(self, context: Message) -> Optional[Any]:
+        return self.troll_message
---
+++ tasks.py
@@ -0,0 +1,7 @@
+from john_community_modules.troll_module import TrollHandler
+
+if __name__ == "__main__":
+    troll = TrollHandler()
+    message = 'Hello John!'
+
+    print(troll(message))
